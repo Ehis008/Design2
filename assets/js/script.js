af974c99a7d6ef0ea13b87408f205101788c743f
Ehis008 <belovedogbu@gmail.com>
2025-03-20 11:20:36 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+const name = "david"
+if (true){
+    const pi=3.142;
+    console.log(pi)
+    console.log (name)
+} 
+
+
+const name2= "Paul Moses";
+const age = 30;
+const has_license = true;
+console.log ("my name is "+name2 +" I am " + age + " years old" + " has license " + has_license);
+console.log(`my name is ${name2}, I am ${age} years old, I have license ${has_license}`)
+
+function sayHello() {
+    console.log("Hello, World!");
+}
+
